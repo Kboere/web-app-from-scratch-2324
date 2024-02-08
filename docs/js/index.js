13ce59d1127715189c27c48584855705f5ca0b34
Kboere <kevin.boere@hva.nl>
2024-02-08 10:46:52 +0100

    added html elements + Fixed header

---
+++ docs/js/index.js
@@ -0,0 +1,113 @@
+// Functie om JSON in te laden met fetch
+function loadJSON() {
+    return fetch('./data/eigenschappen.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Voorbeeld van hoe je de JSON kunt gebruiken om een lijst te maken
+function init() {
+    loadJSON()
+        .then(data => {
+            const outputList = document.getElementById('duckData'); // Get the output list element
+            const eend = data.eend; // Access the "eend" object
+            
+            // Define a mapping object for custom property names
+            const propertyMapping = {
+                "naam": "Naam",
+                "leeftijd": "Leeftijd in eendenjaren",
+                "eendenNaam": "Eenden naam",
+                "karakter": "Karakter van eend",
+                "woonVijver": "Mijn woonvijver",
+                "favorieteBrood": "Favoriete brood",
+                "kwikKwekKwak": "Ik ben een kwik, kwek of kwakker",
+                "favorieteDoelwit": "Mijn favoriete poepdoelwit",
+            };
+
+            // Create a menu container
+            const menuContainer = document.createElement('ul');
+            menuContainer.classList.add('menu-container');
+            
+            // Loop through all properties of the "eend" object
+            for (const key in eend) {
+                if (eend.hasOwnProperty(key)) {
+                    const customPropertyName = propertyMapping[key] || key; // Use custom name if exists, otherwise use original property name
+                    const listItem = document.createElement('li');
+                    listItem.innerHTML = `<span class="property">${customPropertyName}:</span> ${eend[key]}`; // Add custom property name and value to the list item
+                    menuContainer.appendChild(listItem);
+                }
+            }
+
+            // Hide the menu initially
+            menuContainer.style.display = 'none';
+
+            // Append the menu container to the output list
+            outputList.appendChild(menuContainer);
+
+            // Add click event listener to "duckData" element to toggle menu visibility
+            const duckData = document.getElementById('duckData');
+            duckData.addEventListener('click', function() {
+                menuContainer.classList.toggle('active');
+                menuContainer.style.display = (menuContainer.style.display === 'none') ? 'block' : 'none';
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+// Roep de init functie aan zodra de pagina geladen is
+window.onload = init;
+
+
+const parallex_elements = document.querySelectorAll(".para-class");
+
+let xValue = 0;
+let yValue = 0;
+
+const section = document.querySelector('#parallex');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.intersectionRatio >= 0.80) {
+            window.addEventListener("mousemove", handleMove);
+            window.addEventListener("touchmove", handleMove);
+        } else {
+            window.removeEventListener("mousemove", handleMove);
+            window.removeEventListener("touchmove", handleMove);
+        }
+    });
+}, { threshold: 0.80 });
+
+observer.observe(section);
+
+function handleMove(e) {
+    let xValue, yValue;
+    if (e.type === "mousemove") {
+        xValue = e.clientX - window.innerWidth / 2;
+        yValue = e.clientY - window.innerHeight / 2;
+    } else if (e.type === "touchmove") {
+        xValue = e.touches[0].clientX - window.innerWidth / 2;
+        yValue = e.touches[0].clientY - window.innerHeight / 2;
+    }
+
+    console.log(xValue, yValue);
+
+    parallex_elements.forEach((el) => {
+        let speedx = el.dataset.speedx;
+        let speedy = el.dataset.speedy;
+
+        el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+
+    });
+}
+
+
+
